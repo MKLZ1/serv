eabc744f3bd502655e82f8a0e551c9a073455047
jhon4znl <jhon4znl@gmail.com>
2023-03-30 11:27:23 -0500

    xd

--- app.js
+++ app.js
@@ -49,10 +49,10 @@ const corsOptions = {
 //     next();
 // });
 app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 app.use(cookieParser());
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
